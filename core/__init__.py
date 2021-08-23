9529a578fc3a185887aaae4e875102277ba81662
Joules <joules.hdz@gmail.com>
2021-08-23 12:17:12 -0500

    first commit

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+
+
+app = Flask(__name__,  static_folder='../static', template_folder = '../templates')
+
+
+import views
---
+++ views.py
@@ -0,0 +1,6 @@
+from core import app
+from resources import index
+
+
+
+app.route('/')(index)
